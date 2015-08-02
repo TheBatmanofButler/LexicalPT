7d67dffbe2ff20229d1435f29b2dbe3709e3ca9b
Ganesh Ravichandran <gr2483@columbia.edu>
2015-08-01 23:31:25 -0400

    except for reset button, comboboxes are completely finished

--- JS/widgets.js
+++ JS/widgets.js
@@ -1,14 +1,31 @@
 function PatientDateInput(IncomingData) {
 
-  var PatientData = {}
+  var PatientData = {['']:['']}
+  var DateData = ['']
   $.each(IncomingData.Items, function() {
-    PatientData[this['patient']['S']] = [this['apptDate']['N']];
+    t_ms = this['apptDate']['N']
+    patientName = this['patient']['S']
+
+    var t_utc = new Date(parseInt(t_ms));
+    var t_formatted = t_utc.toDateString()
+    if (patientName in PatientData) {
+      PatientData[patientName].push(t_formatted);
+    } else {
+      PatientData[patientName] = [t_formatted];
+    }
+
+    DateData.push(t_formatted);
   });
 
   $("#patient_combobox").select2({
     placeholder: "Select a Patient",
     data: Object.keys(PatientData)
   });
+
+  $("#date_combobox").select2({
+    placeholder: "Select a Date",
+    data: DateData
+    });
 
   // var dateList = []
   // for (key in IncomingData) {
@@ -26,14 +43,64 @@ function PatientDateInput(IncomingData) {
 
     var dateData = [];
     for (var date in PatientData[$patientName]) {
-        t_ms = PatientData[$patientName][date];
-        var t_utc = new Date(parseInt(t_ms));
-        dateData.push(t_utc.toDateString());
+        dateData.push(PatientData[$patientName][date]);
       }
-
+    if (dateData.length > 1) {
+      dateData.unshift('');
+    }
     $("#date_combobox").select2({
+      placeholder: "Select a Date",
       data: dateData
     });
+
+    if ($("#patient_combobox").val() != '' && $("#date_combobox").val() != '') {
+      $("#patient_combobox, #date_combobox").prop( "disabled", true );
+      $("#queryResetButton").show();
+    }
+
+  });
+
+  $("#date_combobox").change( function() {
+    var $patientDate = $("#date_combobox").val();
+    $("#patient_combobox").children().remove().end();
+
+    var patientData = [];
+    for (var patientName in PatientData) {
+        if (PatientData[patientName].indexOf($patientDate) >= 0) {
+          patientData.push(patientName);
+        }
+      }
+    if (patientData.length > 1) {
+      patientData.unshift('');
+    }
+    $("#patient_combobox").select2({
+      placeholder: "Select a Patient",
+      data: patientData
+    });
+
+  // if ($("#patient_combobox").val() != '' && $("#date_combobox").val() != '') {
+  //   $("#patient_combobox, #date_combobox").prop( "disabled", true );
+  //   $("#queryResetButton").show();
+  // }
+
+  // $("#queryResetButton").click( function() {
+
+  //   $("#patient_combobox").val('');
+  //   $("#patient_combobox").select2({
+  //     placeholder: "Select a Patient",
+  //     data: Object.keys(PatientData)
+  //   });
+
+  //   $("#date_combobox").val('');
+  //   $("#date_combobox").select2({
+  //     placeholder: "Select a Date",
+  //     data: DateData
+  //   });
+
+  //   $("#patient_combobox, #date_combobox").prop( "disabled", false );
+  //   $("#queryResetButton").hide();
+  // });
+
   });
 
   // $("#date_combobox").change( function() {
