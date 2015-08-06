b6288bc14b1be04d4b26583cc29b0b5895294cdf
Amol Kapoor <theahura@gmail.com>
2015-08-06 19:42:14 -0400

    Updated create form bug
    Signed-off-by: Amol Kapoor <theahura@gmail.com>

--- JS/files.js
+++ JS/files.js
@@ -154,7 +154,7 @@ function createForm() {
 
     $(".multi-day-form-exercises-info-container").append($form);
 
-    $(".tables .multi-day-form-exercises-info-container").fadeIn();
+    $(".tables, .multi-day-form-exercises-info-container").fadeIn();
 }
 
 
