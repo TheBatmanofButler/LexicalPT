cd45ec0d93266f86a5d826d9f17df0d2f32bf270
Amol Kapoor <theahura@gmail.com>
2015-08-21 01:42:39 -0400

    Fixed pdf extra page, added footer
    Signed-off-by: Amol Kapoor <theahura@gmail.com>

--- JS/files_settings.js
+++ JS/files_settings.js
@@ -103,6 +103,10 @@ function downloadFormsAsPDF() {
 
     for(var formGroup = 0; formGroup < formList.length; formGroup+=formsPerPage) {
         
+        if(formGroup > 0) {
+            doc.addPage();
+        }
+
         var yStart = 50;
         var xStart = 20;
 
@@ -124,7 +128,7 @@ function downloadFormsAsPDF() {
             
         }
 
-        doc.addPage()
+        doc.text(xStart, 820, "Powered by Diagraphic Technologies");
     }
 
     //$.when.apply($, deferredArray).then(function() {
