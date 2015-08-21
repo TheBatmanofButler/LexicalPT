5f6ee9b0e49db1ffb9447344ba4353d1086a0e50
Ganesh Ravichandran <gr2483@columbia.edu>
2015-08-21 01:10:16 -0400

    date format alert

--- JS/files_misc_helper.js
+++ JS/files_misc_helper.js
@@ -44,13 +44,51 @@ function attachSubmitHandler(formId) {
 }
 
 /**
+ * isValidDate(str)
+ * @param string str value yyyy-mm-dd
+ * @return boolean true or false
+ * IF date is valid return true
+ */
+function isValidDate(str){
+    // STRING FORMAT yyyy-mm-dd
+    if(str=="" || str==null){return false;}                             
+
+    // m[1] is year 'YYYY' * m[2] is month 'MM' * m[3] is day 'DD'                  
+    var m = str.match(/(\d{4})-(\d{2})-(\d{2})/);
+
+    // STR IS NOT FIT m IS NOT OBJECT
+    if( m === null || typeof m !== 'object'){return false;}             
+
+    // CHECK m TYPE
+    if (typeof m !== 'object' && m !== null && m.size!==3){return false;}
+                
+    var ret = true; //RETURN VALUE                      
+    var thisYear = new Date().getFullYear(); //YEAR NOW
+    var minYear = 1999; //MIN YEAR
+
+    // MONTH CHECK          
+    if( (m[2].length < 2) || m[2] < 1 || m[2] > 12){ret = false;}
+    // DAY CHECK
+    if( (m[3].length < 2) || m[3] < 1 || m[3] > 31){ret = false;}
+
+    return ret;         
+}
+
+/**
     Searches a form for basic errors regarding patient name and date, and prompts user if errors are found
 
     @param: form; string; id of form WITH a # 
 */
 function checkFormErrors(form) {
 
-    var date = new Date($(form + " .apptDate").val());
+    var dateInput = $(form + " .apptDate").val();
+
+    if(!isValidDate(dateInput)) {
+        alert('Date must be in format YYYY-MM-DD.');
+        return false;
+    }
+
+    var date = new Date(dateInput);
 
     var name = $(".meta-data .patient_last").val().toUpperCase() + ", " + $(".meta-data .patient_first").val().toUpperCase();
 
